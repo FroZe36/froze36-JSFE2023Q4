5990508ad657254ba15092ae066002b9902fc69f
Danila <AngelofDest1ny@yandex.ru>
2024-01-15 15:51:11 +0300

    feat: add work gallow with word and work keyboard

--- hangman/index.js
+++ hangman/index.js
@@ -1,41 +1,43 @@
 'use strict';
-//GAME
+//LAYOUT
 const container = document.createElement('div');
-container.className = 'container'
+container.className = 'container';
 document.body.append(container);
 
 const gallowWrapper = document.createElement('div');
-gallowWrapper.className = 'gallow-wrapper'
+gallowWrapper.className = 'gallow-wrapper';
 const gallowName = document.createElement('h1');
 const gallowImage = document.createElement('img');
-gallowImage.src = './assets/img/gallows.svg';
+gallowImage.className = 'gallow-image';
+gallowImage.src = './assets/img/hangman-0.svg';
 gallowName.textContent = 'Hangman Game';
 container.append(gallowWrapper);
 gallowWrapper.append(gallowName, gallowImage);
 
 const gameWrapper = document.createElement('div');
-gameWrapper.className = 'game-wrapper'
+gameWrapper.className = 'game-wrapper';
 container.append(gameWrapper);
 const letterList = document.createElement('ul');
-letterList.className = 'letter-list'
+letterList.className = 'letter-list';
 const hintText = document.createElement('h2');
-hintText.className = 'hint'
+hintText.className = 'hint';
 const spanHintText = document.createElement('span');
-hintText.textContent = 'Hint:';
-hintText.append(spanHintText)
+hintText.textContent = 'Hint: ';
+hintText.append(spanHintText);
 const guessesText = document.createElement('h2');
-guessesText.className = 'guesses'
+guessesText.className = 'guesses';
 const spanGuessesText = document.createElement('span');
-guessesText.textContent = 'Incorrect guesses:';
-guessesText.append(spanGuessesText)
+guessesText.textContent = 'Incorrect guesses: ';
+guessesText.append(spanGuessesText);
 
 const keyboard = document.createElement('div');
-keyboard.className = 'keyboard'
+keyboard.className = 'keyboard';
 const alphabet = 'abcdefghijklmnopqrstuvwxyz';
-for(let i = 0; i < alphabet.length; i++) {
+for (let i = 0; i < alphabet.length; i++) {
   const btn = document.createElement('button');
   btn.textContent = alphabet[i];
   keyboard.append(btn);
+  btn.addEventListener('click', e => gamePlay(e.target, alphabet[i]));
 }
 gameWrapper.append(letterList, hintText, guessesText, keyboard);
 
@@ -44,14 +46,58 @@ const modal = document.createElement('div');
 modal.className = 'modal';
 const modalContent = document.createElement('div');
 const modalResult = document.createElement('h3');
-modalResult.textContent = 'Game Over!'
+modalResult.textContent = 'Game Over!';
 const modalSecretWord = document.createElement('p');
 const spanSecretWord = document.createElement('span');
-spanSecretWord.textContent = 'Barabedsadasda'
-modalSecretWord.textContent = 'The word was: '
+spanSecretWord.textContent = 'Barabedsadasda';
+modalSecretWord.textContent = 'The word was: ';
 modalSecretWord.append(spanSecretWord);
 const modalBtn = document.createElement('button');
-modalBtn.textContent = 'Play again'
-modalContent.append(modalResult, modalSecretWord, modalBtn)
-modal.append(modalContent)
+modalBtn.textContent = 'Play again';
+modalContent.append(modalResult, modalSecretWord, modalBtn);
+modal.append(modalContent);
 document.body.append(modal);
+
+//GAME
+document.addEventListener('keydown', e =>
+  gamePlay(
+    Array.from(keyboard.querySelectorAll('button')).filter(
+      item => item.textContent === e.key,
+    )[0],
+    e.key,
+  ),
+);
+let currentWord;
+let wrongCount = 0;
+let currentLetters = [];
+const maxWrongGuesses = 6;
+spanGuessesText.textContent = `${wrongCount} / ${maxWrongGuesses}`;
+
+function getRandomWord() {
+  const { word, hint } = wordList[Math.floor(Math.random() * wordList.length)];
+  currentWord = word;
+  spanHintText.textContent = hint;
+  console.log(word, hint);
+  letterList.innerHTML = word
+    .split('')
+    .map(() => `<li></li>`)
+    .join('');
+}
+function gamePlay(target, letter) {
+  if (currentWord.includes(letter)) {
+    [...currentWord].forEach((item, i) => {
+      if (item === letter) {
+        currentLetters.push(letter);
+        letterList.querySelectorAll('li')[i].textContent = letter;
+        letterList.querySelectorAll('li')[i].className = 'guessed';
+      }
+    });
+  } else {
+    wrongCount++;
+    gallowImage.src = `./assets/img/hangman-${wrongCount}.svg`;
+    spanGuessesText.textContent = `${wrongCount} / ${maxWrongGuesses}`;
+  }
+  target.disabled = true;
+}
+
+getRandomWord();
