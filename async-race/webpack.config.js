34e5abc2ff6ec8b391fc25b5eecaa76d6f9ae430
Danila <AngelofDest1ny@yandex.ru>
2024-03-26 14:56:52 +0300

    chore: inital all needs files. Setup webpack, eslint, typescript, prettier and configure them

---
+++ async-race/webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const EslintPlugin = require('eslint-webpack-plugin');
+
+const baseConfig = {
+  entry: path.resolve(__dirname, './src/index.ts'),
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.ts/i,
+        use: 'ts-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.ts']
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, './dist'),
+    assetModuleFilename: 'assets/[name]-[hash][ext]'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/index.html'),
+      filename: 'index.html'
+    }),
+    new CopyWebpackPlugin({
+      // patterns: [
+      //   {
+      //     from: path.resolve(__dirname, 'src/rss-puzzle-data'),
+      //     to: path.resolve(__dirname, 'dist/rss-puzzle-data')
+      //   }
+      // ]
+    }),
+    new CleanWebpackPlugin(),
+    new EslintPlugin({ extensions: 'ts' })
+  ]
+};
+
+module.exports = ({ mode }) => {
+  const isProductionMode = mode === 'prod';
+  const envConfig = isProductionMode ? require('./webpack.prod.config') : require('./webpack.dev.config');
+
+  return merge(baseConfig, envConfig);
+};
